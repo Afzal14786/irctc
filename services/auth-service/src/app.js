c6cfb9bc1714d2bdc6106fb50ec65066f4c5857b
Afzal14786 <mdafzal14777@gmail.com>
2026-09-02 02:27:38 +0530

    integrate logger inside auth service and tested

--- services/auth-service/src/app.js
+++ services/auth-service/src/app.js
@@ -1,6 +1,16 @@
 import express from "express";
 
+import logger from "./config/logger.js";
+import { request_logger_middleware } from "@irctc/logger";
+
 const app = express();
+
+app.use(express.json());
+app.use(request_logger_middleware(logger));
+
+app.get("/", (req, res) => {
+    res.send(`Hello from authentication service darling`);
+});
 
 
 export default app;
--- services/auth-service/src/server.js
+++ services/auth-service/src/server.js
@@ -1,14 +1,14 @@
 import app from "./app.js";
 import {env} from "./config/env.js";
+import logger from "./config/logger.js";
 
 const PORT = env.AUTH_SERVICE_PORT;
-const ENVIRONMENT = env.NODE_ENV;
-const SERVICE_NAME = env.AUTH_SERVICE;
 
 const server = app.listen(PORT, ()=> {
-    console.info(`The [${SERVICE_NAME}] is running on port ${PORT} in ${ENVIRONMENT}`);
+    logger.info(`Authentication Service | Server Running on ${PORT}`);
 });
 
 server.on("error", (err) => {
-    console.error(`error while running ${SERVICE_NAME}`);
+    logger.error({err: err}, "error while running authentication service");
+    process.exit(1);
 });
