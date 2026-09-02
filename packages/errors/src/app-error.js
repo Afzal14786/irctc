c42cbd9d87ba9e6c1fcee21b90d9e5914e1a5bcb
Afzal14786 <mdafzal14777@gmail.com>
2026-09-02 13:01:24 +0530

    update error packages inside @irctc/errors

--- packages/errors/src/app-error.js
+++ packages/errors/src/app-error.js
@@ -1,11 +1,27 @@
-import {HTTP_CODES} from "@irctc/http";
+import { HTTP_CODES } from "@irctc/http";
 
 export class AppError extends Error {
-  constructor(message, statusCode = HTTP_CODES.INTERNAL_SERVER_ERROR, isOperational = true) {
-    super(message);
-    this.name = this.constructor.name;
-    this.statusCode = statusCode;
-    this.isOperational = isOperational; 
-    Error.captureStackTrace(this, this.constructor);
-  }
+    constructor(
+        message,
+        status_code = HTTP_CODES.INTERNAL_SERVER_ERROR,
+        is_operational = true
+    ) {
+        super(message);
+
+        this.name = this.constructor.name;
+
+        this.status_code = status_code;
+
+        this.status =
+            status_code >= 400 && status_code < 500
+                ? "FAIL"
+                : "ERROR";
+
+        this.is_operational = is_operational;
+
+        Error.captureStackTrace(
+            this,
+            this.constructor
+        );
+    }
 }
---
+++ packages/errors/src/index.js
@@ -0,0 +1,6 @@
+export * from "./app-error.js";
+export * from "./ValidationError.js";
+export * from "./UnauthorizedError.js";
+export * from "./NotFoundError.js";
+export * from "./ForbiddenError.js";
+export * from "./ConflictError.js";
